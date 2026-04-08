8823a3698429f1dbeacf4a747b245cb853d3b7ba
ezekiel <favourtobiloba200@gmail.com>
2026-04-08 14:09:26 +0100

    all files

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Button = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("hello")}>add</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const links = [
+    { title: "Home", route: "/" },
+    { title: "about", route: "about" },
+    { title: "contact", route: "contact" },
+    { title: "Dashboard", route: "dashboard" },
+  ];
+  return (
+    <div>
+      <ul className="flex w-[50%] m-auto justify-between p-4 border border-amber-50 rounded-full mt-5">
+        {links.map((link, i) => (
+          <Link
+            href={link.route}
+            className="hover:bg-white hover:text-black p-2 rounded-full cursor-pointer "
+            key={i}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
